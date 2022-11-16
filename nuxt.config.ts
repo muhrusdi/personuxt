4d0f984f2731ff6176a150dd1e44d7d8f680a838
muhrusdi <mhmrofficial@gmail.com>
2022-11-17 00:20:18 +0700

    chore: add inter ui font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Sun.js',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'Nuxt.js boilerplate' }
+      ],
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://rsms.me/"
+        },
+        {
+          rel: "stylesheet",
+          href: "https://rsms.me/inter/inter.css"
+        },
+      ]
+    }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
