0f4bec15d0e6fe6a232c393b33354614a4e41162
muhrusdi <mhmrofficial@gmail.com>
2022-11-17 00:31:02 +0700

    chore: update overview component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
+    "./templates/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
--- templates/overview/index.vue
+++ templates/overview/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <h1>teasdfst</h1>
+  <div class="min-h-screen grid place-items-center bg-gray-900">
+    <div class="text-center">
+      <h1 class="font-bold text-4xl text-white">Sun.js</h1>
+      <p class="mt-4 text-gray-400">The Nuxt.js boilerplate</p>
+    </div>
   </div>
 </template>
