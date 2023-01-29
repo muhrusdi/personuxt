dde67d2f7f4fddf761e0c28cb485db923adbf1f6
muhrusdi <mhmrofficial@gmail.com>
2023-01-30 00:18:24 +0700

    add nav component

---
+++ components/navigation/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nav class="fixed w-[260px] bg-[#0f172a] h-screen">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </nav>
+    
+  </div>
+</template>
