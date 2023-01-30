8cd3e5da59917bfd10277834776395b83cdc436e
muhrusdi <mhmrofficial@gmail.com>
2023-01-30 15:50:35 +0700

    add runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,4 +33,12 @@ export default defineNuxtConfig({
   css: [
     '/styles/global.css',
   ],
+  runtimeConfig: {
+    // Private keys are only available on the server
+    apiSecret: '123',
+    // Public keys that are exposed to the client
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
+    }
+  }
 })
