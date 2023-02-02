b13328818d713a99535621133b8166a8e9eb0704
muhrusdi <mhmrofficial@gmail.com>
2023-02-02 16:24:19 +0700

    update interceptors

--- composables/api/index.ts
+++ composables/api/index.ts
@@ -6,22 +6,20 @@ const $$fetch = <TOptions>(url: string, options: TOptions) => {
   const config = useRuntimeConfig()
   return $fetch(url, {
     baseURL: config.API_URL,
+    async onRequest(ctx: any) {
+      const accessToken = await useCookie('access_token' ,{ default: undefined })
+
+      if (undefined !== accessToken) {
+          ctx.options.headers = new Headers(ctx.options.headers)
+          ctx.options.headers.append('Authorization', accessToken.value)
+          // ctx.options.headers.set('Authorization', accessToken.value)
+          // ctx.options.headers['Authorization'] = `Bearer ${accessToken.value}`
+      }
+    },
     ...options
   })
 }
 
 const useSampleAPI = <TProps>({options}: VariableTypes<TProps>) => {
-  return useAsyncData('get-api', () => $$fetch("/api", {
-    async onRequest(ctx: any) {
-        const accessToken = await useCookie('access_token' ,{ default: undefined })
-
-        if (undefined !== accessToken) {
-            ctx.options.headers = new Headers(ctx.options.headers)
-            ctx.options.headers.append('Authorization', accessToken.value)
-            // ctx.options.headers.set('Authorization', accessToken.value)
-            // ctx.options.headers['Authorization'] = `Bearer ${accessToken.value}`
-        }
-    },
-      ...options
-  }))
+  return useAsyncData('get-api', () => $$fetch("/api", options))
 }
