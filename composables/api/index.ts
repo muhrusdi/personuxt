faf440f87b0e9c1ade0ebb5370653f9292c58d20
muhrusdi <mhmrofficial@gmail.com>
2023-02-02 16:21:26 +0700

    custom $fetch

--- composables/api/index.ts
+++ composables/api/index.ts
@@ -1,18 +1,27 @@
-const useSampleAPI = ({options}: any) => {
+type VariableTypes<TOptions> = {
+  options: TOptions
+}
+
+const $$fetch = <TOptions>(url: string, options: TOptions) => {
   const config = useRuntimeConfig()
+  return $fetch(url, {
+    baseURL: config.API_URL,
+    ...options
+  })
+}
 
-  return useAsyncData('get-api', () => $fetch("/api", {
-      baseURL: config.API_URL,
-      async onRequest(ctx: any) {
-          const accessToken = await useCookie('access_token' ,{ default: undefined })
+const useSampleAPI = <TProps>({options}: VariableTypes<TProps>) => {
+  return useAsyncData('get-api', () => $$fetch("/api", {
+    async onRequest(ctx: any) {
+        const accessToken = await useCookie('access_token' ,{ default: undefined })
 
-          if (undefined !== accessToken) {
-              ctx.options.headers = new Headers(ctx.options.headers)
-              ctx.options.headers.append('Authorization', accessToken.value)
-              // ctx.options.headers.set('Authorization', accessToken.value)
-              // ctx.options.headers['Authorization'] = `Bearer ${accessToken.value}`
-          }
-      },
+        if (undefined !== accessToken) {
+            ctx.options.headers = new Headers(ctx.options.headers)
+            ctx.options.headers.append('Authorization', accessToken.value)
+            // ctx.options.headers.set('Authorization', accessToken.value)
+            // ctx.options.headers['Authorization'] = `Bearer ${accessToken.value}`
+        }
+    },
       ...options
   }))
 }
