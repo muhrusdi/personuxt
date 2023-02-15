83dc74aaed1173df35a781510d47bcb474f0af88
muhrusdi <mhmrofficial@gmail.com>
2023-02-15 13:16:56 +0700

    feat: add store directory

--- templates/overview/index.vue
+++ templates/overview/index.vue
@@ -1,8 +1,16 @@
+<script setup>
+  import { useCounterStore } from '~/stores/counter';
+
+  const counter = useCounterStore();
+  
+</script>
+
 <template>
   <div class="min-h-screen grid place-items-center bg-gray-900">
     <div class="text-center">
       <h1 class="font-bold text-4xl text-white">Sun.js</h1>
       <p class="mt-4 text-gray-400">The Nuxt.js boilerplate</p>
+      <button class="bg-pink-600 py-2 px-4 rounded mt-4" @click="counter.increment">Click {{ counter.count }}</button>
     </div>
   </div>
 </template>
