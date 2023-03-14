0bb411cbff597f1f2817782001b5091c995c5c53
muhrusdi <mhmrofficial@gmail.com>
2023-03-15 00:04:52 +0700

    bugfix: update comp

---
+++ components/forms/input/input.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { useField } from 'vee-validate';
+  import { defineProps, toRef } from 'vue';
+
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true
+    }
+  })
+  const nameRef = toRef(props, "name")
+  const { errorMessage, value } = useField(nameRef)
+</script>
+
+<template>
+  <input v-model="value" type="text">
+  <span>{{ errorMessage }}</span>
+</template>
--- templates/overview/index.vue
+++ templates/overview/index.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
   import { useCounterStore } from '~/stores/counter';
+  import { Form, Field } from "vee-validate"
+  import Input from "~/components/forms/input/input.vue"
 
   const counter = useCounterStore();
+
+  const handleSubmit = (e: any) => {
+    console.log("---", e) 
+  }
 
 </script>
 
@@ -9,6 +15,10 @@
   <div class="min-h-screen grid place-items-center bg-gray-900">
     <div class="text-center">
       <h1 class="font-bold text-4xl text-white">Sun.js</h1>
+      <Form @submit="handleSubmit">
+        <Input name="email"/>
+        <button>submit</button>
+      </Form>
       <p class="mt-4 text-gray-400">The Nuxt.js boilerplate</p>
       <button class="bg-pink-600 py-2 px-4 rounded mt-4" @click="counter.increment">Click {{ counter.count }}</button>
     </div>
