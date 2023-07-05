e5633fa7b3f854e8caaa08d9804a08e3be14d36a
muhrusdi <mhmrofficial@gmail.com>
2023-07-05 14:13:15 +0700

    chore: add env file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,55 +2,49 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-16',
-      viewport: 'width=500, initial-scale=1',
-      title: 'Sun.js',
+      charset: "utf-16",
+      viewport: "width=500, initial-scale=1",
+      title: "Sun.js",
       meta: [
         // <meta name="description" content="My amazing site">
-        { name: 'description', content: 'Nuxt.js boilerplate' }
+        { name: "description", content: "Nuxt.js boilerplate" },
       ],
       link: [
         {
           rel: "preconnect",
-          href: "https://rsms.me/"
+          href: "https://rsms.me/",
         },
         {
           rel: "stylesheet",
-          href: "https://rsms.me/inter/inter.css"
+          href: "https://rsms.me/inter/inter.css",
         },
-      ]
-    }
+      ],
+    },
   },
   experimental: {
-    componentIslands: true
+    componentIslands: true,
   },
   postcss: {
     plugins: {
-      'tailwindcss/nesting': {},
+      "tailwindcss/nesting": {},
       tailwindcss: {},
       autoprefixer: {},
-      'postcss-hexrgba': {},
-      'postcss-lighten-darken': {},
+      "postcss-hexrgba": {},
+      "postcss-lighten-darken": {},
     },
   },
-  css: [
-    '/styles/global.css',
-  ],
+  css: ["/styles/global.css"],
   runtimeConfig: {
     // Private keys are only available on the server
-    apiSecret: '123',
+    apiSecret: "",
     // Public keys that are exposed to the client
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
-    }
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "/api",
+    },
   },
-  modules: [
-    '@pinia/nuxt',
-    '@nuxt/devtools',
-    '@privyid/persona'
-  ],
+  modules: ["@pinia/nuxt", "@nuxt/devtools", "@privyid/persona"],
   devtools: {
     enabled: true,
     vscode: {},
-  }
+  },
 })
