a987fff626e02d3d759dad1b25387a66a6000eeb
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2023-07-06 13:50:29 +0700

    design: layout sidebar

--- components/navigation/index.vue
+++ components/navigation/index.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>
-    <nav class="fixed w-[260px] bg-[#0f172a] text-base-white h-screen">
-      <ul class="list-none">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <p-button>Click me</p-button>
-        </li>
-      </ul>
-    </nav>
-  </div>
+  <p-sidebar fixed>
+    <template #brand>
+      <p-sidebar-brand>
+        <h1>LOGO</h1>
+      </p-sidebar-brand>
+    </template>
+    <p-sidebar-nav>
+      <p-nav-item active>
+        <template #icon>
+          <PiDashboard16 />
+        </template>
+        Dashboard
+      </p-nav-item>
+      <p-nav-sub-item text="Documents" collapsible>
+        <template #icon>
+          <PiDocumentFilled16 />
+        </template>
+        <p-sidebar-nav>
+          <p-nav-item>
+            Need Action
+          </p-nav-item>
+          <p-nav-item>
+            In Progress
+          </p-nav-item>
+          <p-nav-item>
+            Others
+          </p-nav-item>
+        </p-sidebar-nav>
+      </p-nav-sub-item>
+    </p-sidebar-nav>
+  </p-sidebar>
 </template>
+
+<script lang="ts" setup>
+import PiDocumentFilled16 from '@privyid/persona-icon/vue/document-filled/16.vue'
+import PiDashboard16 from '@privyid/persona-icon/vue/dashboard/16.vue'
+</script>
